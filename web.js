6ed040a24c4acfc918a7e51202ffb013f23b30f7
Ubuntu <ubuntu@ip-172-31-14-236.us-west-2.compute.internal>
2013-07-09 01:54:15 +0000

    updated

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
+
+var contents = fs.readFileSync('index.html');
+
+console.log("String on file is " + contents);
+
+var buffer = new Buffer(contents);
+var encodded = buffer.toString();
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(encodded);
 });
 
 var port = process.env.PORT || 5000;
